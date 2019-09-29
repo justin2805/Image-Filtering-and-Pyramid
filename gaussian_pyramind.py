111966403c59c8a29a78cd71711ac1d1517644b7
justin <justin2805@gmail.com>
2019-09-29 02:16:40 -0500

    gaussian

--- gaussian_pyramind.py
+++ gaussian_pyramind.py
@@ -13,13 +13,13 @@ class Gaussian_pyramind:
         original_image = read_disp_img.Read_display_images().readImage()
         final_image = original_image
         image_array = [final_image]
-        resized_img_array = [cv2.resize(original_image,(96,96))]
+        resized_img_array = [cv2.resize(original_image,(109,109))]
         # print(final_image[0])
         # print("\n***********************************************************\n")
         for i in range(6):
             final_image = cv2.pyrDown(final_image)
             image_array.append(final_image)
-            resized_img_array.append(cv2.resize(final_image,(96,96)))
+            resized_img_array.append(cv2.resize(final_image,(109,109)))
             # print(final_image[0])
             # print("\n***********************************************************\n")
             # self.i_o_img().display_image(original_image, final_image)
@@ -30,19 +30,22 @@ class Gaussian_pyramind:
         print("cols : ",cols)
         print(cols + cols // 2)
         # pyramid = tuple(pyramid_gaussian(image, downscale=2, multichannel=True))
-        composite_image = numpy.zeros((rows+96, cols + cols // 2), dtype=numpy.double)
+        composite_image = numpy.zeros((rows+109, cols + cols // 2), dtype=numpy.double)
         print(composite_image.shape)
-        composite_image[:96,:96] = resized_img_array[0]
+        print(resized_img_array[0].shape)
+        composite_image[:109,:109] = resized_img_array[0]
         r_row = 0
-        r_col = 96
+        r_col = 109
         for p in resized_img_array[1:]:
             n_rows, n_cols = p.shape[:2]
             composite_image[r_row: r_row+n_rows,r_col: r_col+n_cols] = p
-            r_col = r_col + 96
+            r_col = r_col + 109
 
-        print("row : ",rows)
-        composite_image[:rows, :cols] = image_array[0]
-        i_row = 96
+
+        row = 109
+        print("row : ",row)
+        composite_image[row:row+rows, :cols] = image_array[0]
+        i_row = 109
         for p in image_array[1:]:
             n_rows, n_cols = p.shape[:2]
             print("n_rows n_cols ",n_rows)
