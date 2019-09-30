947a2d092b0731272a31452d78cb1e3d560adadf
justin <justin2805@gmail.com>
2019-09-29 23:54:01 -0500

    corrections/comments

--- gaussian_pyramind.py
+++ gaussian_pyramind.py
@@ -9,7 +9,7 @@ class Gaussian_pyramind:
         return read_disp_img.Read_display_images()
 
     def make_pyramid(self):
-        original_image = read_disp_img.Read_display_images().readImage(0)
+        original_image = read_disp_img.Read_display_images().readImage(5)
         final_image = original_image
         image_array = [final_image]
         resized_img_array = [cv2.resize(original_image,(109,109))]
@@ -29,6 +29,10 @@ class Gaussian_pyramind:
         # print("cols : ",cols)
         # print(cols + cols // 2)
         # pyramid = tuple(pyramid_gaussian(image, downscale=2, multichannel=True))
+
+        # composite_image is an array that stores all the images in a specific format.
+        # The format contains information on where the image is to be displayed in the plot
+        # First the array is initialized with fixed dimension - which is filled with zero
         composite_image = numpy.zeros((rows+150, cols + cols // 2), dtype=numpy.double)
         # print(composite_image.shape)
         # print(resized_img_array[0].shape[:1])
@@ -40,8 +44,10 @@ class Gaussian_pyramind:
         text_post_y = 135
 
         fig, ax = plt.subplots()
+        # display text equal to image resolution below each each on the upper horizontal layer
         ax.text(text_post_x,text_post_y,image_array[0].shape[1],fontsize=10, color="white")
 
+        # To add images for the upper horizontal layer of the gaussian pyramid
         for p in resized_img_array[1:]:
             n_rows, n_cols = p.shape[:2]
             composite_image[r_row: r_row+n_rows,r_col: r_col+n_cols] = p
@@ -56,6 +62,7 @@ class Gaussian_pyramind:
             n_rows, n_cols = p.shape[:2]
             # print("n_rows n_cols ",n_rows)
             composite_image[i_row:i_row + n_rows, cols:cols + n_cols] = p
+            # display text equal to image resolution below each each on the upper horizontal layer
             text_post_x += 109
             ax.text(text_post_x, text_post_y, p.shape[1], fontsize=10, color="white")
             # print("n_rows : ",n_rows)
